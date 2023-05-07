f988934dee83fe21c32482b1dbe7c710f48fb24b
Malte Hückstädt <47598771+MalteHueckstaedt@users.noreply.github.com>
2023-05-07 19:02:31 +0200

    Create Nodes_SB_further_info.r

---
+++ posts/Instagram_R/Nodes_SB_further_info.r
@@ -0,0 +1,17 @@
+Magazine # kaleidoscopemagazine 71.000 Follower
+Projekraum #spaziomaiocchi
+Musiker #torvs 9.104 Follower
+Künstler #cavecanems 60.300 Follower
+Musiker #kevkoko 30.700 Follower
+Member #josephinekinsey 20.500 Follower
+Musiker #bill_kouligas 18.500 Follower
+Company #bam_wrks 18.500 Follower -> Kommunikationsagentur
+Member #denis_olgac 8.455 Follower
+Member #lukasolgac 5.494 Follower
+Member #alessandro.belliero 4.002 Follower
+Home #sucukundbratwurst 107.000 Follower
+CEO #alessio_ascari 13.400 Follower
+Künstler #nikkosmas 11.500 Follower
+Member #david__goenner 1.669 Follower
+Company #nike Anzahl 290.000.000
+Musiker #evianchrist 13.200 Follower
