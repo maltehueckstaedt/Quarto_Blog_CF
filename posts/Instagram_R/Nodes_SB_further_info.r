e242d29498ddf2bb07f853814637a1e46daf15f5
Malte Hückstädt <47598771+MalteHueckstaedt@users.noreply.github.com>
2023-05-08 16:44:04 +0200

    Update Nodes_SB_further_info.r

--- posts/Instagram_R/Nodes_SB_further_info.r
+++ posts/Instagram_R/Nodes_SB_further_info.r
@@ -1,107 +1,107 @@
-Magazine # kaleidoscopemagazine 71.000 Follower
-Projekraum #spaziomaiocchi
-Musiker #torvs 9.104 Follower
-Künstler #cavecanems 60.300 Follower
-Musiker #kevkoko 30.700 Follower
+Company # kaleidoscopeCompany 71.000 Follower
+Company #spaziomaiocchi
+Musician #torvs 9.104 Follower
+Artist #cavecanems 60.300 Follower
+Musician #kevkoko 30.700 Follower
 Member #josephinekinsey 20.500 Follower
-Musiker #bill_kouligas 18.500 Follower
+Musician #bill_kouligas 18.500 Follower
 Company #bam_wrks 18.500 Follower -> Kommunikationsagentur
 Member #denis_olgac 8.455 Follower
 Member #lukasolgac 5.494 Follower
 Member #alessandro.belliero 4.002 Follower
 Home #sucukundbratwurst 107.000 Follower
-CEO #alessio_ascari 13.400 Follower
-Künstler #nikkosmas 11.500 Follower
+Company_Staff #alessio_ascari 13.400 Follower
+Artist #nikkosmas 11.500 Follower
 Member #david__goenner 1.669 Follower
 Company #nike Anzahl 290.000.000
-Musiker #evianchrist 13.200 Follower
-Künstler #iosifabaab 2.391 Follower
-NA #hendrik.schneider 22.200 Follower ??????? ??????? ??????? ??????? ???????
+Musician #evianchrist 13.200 Follower
+Artist #iosifabaab 2.391 Follower
+Other #hendrik.schneider 22.200 Follower ??????? ??????? ??????? ??????? ???????
 Company #highsnobiety 5.100.000 Follower
-Musiker #robertdietz 17.100 Follower
+Musician #robertdietz 17.100 Follower
 Company #alexanderwangny 5.600.000 Follower
 Company_Staff #maisie_willoughby 5.609 Follower
 Company #studioteiwas 1.926 Follower
 Company #umbro 726.000 Follower
 Company #highsnobietyshop 142.000 Follower
-Künstler #danielsannwald 92.200 Follower
+Artist #danielsannwald 92.200 Follower
 Company #koeniggalerie 286.000 Follower
 Company_Staff #johann.koenig 102.000 Follower
 Company #nikesportswear 7.800.000 Follower
 Company_Staff #kameronkubicki 2.878 Follower
 Company_Staff #alexwangny 500.000 Follower
-Musiker #brat_star 16.500 Follower
-NA #simonxkaiser 10.500  Follower
-Künstler #joeriwoudstra 2.928 Follower
+Musician #brat_star 16.500 Follower
+Other #simonxkaiser 10.500  Follower
+Artist #joeriwoudstra 2.928 Follower
 Company #amp_yourself 6.062 Follower
-Musiker #y.hurn 586.000 Follower
-Künstler #dis 96.200 Follower
+Musician #y.hurn 586.000 Follower
+Artist #dis 96.200 Follower
 Company #dazedbeauty 390.000 Follower
-Künstler #isamayaffrench 368.000 Follower
+Artist #isamayaffrench 368.000 Follower
 Company #dazed 1.900.000 Follower
-Künstler #maximeballesteros 51.000 Follower
+Artist #maximeballesteros 51.000 Follower
 Company #footlocker 12.000.000 Follower
 Company #footlockereu 2.300.000 Follower
-Künstler #nevenallgeier 12.100 Follower
+Artist #neveOtherllgeier 12.100 Follower
 Company #vice 5.000.000 Follower
-Musiker #max_filesize 1.182 Follower
-Musiker #rintintin 614.000 Follower
-Musiker #minhtendo 40.100 Follower
+Musician #max_filesize 1.182 Follower
+Musician #rintintin 614.000 Follower
+Musician #minhtendo 40.100 Follower
 Company #division.recordings 6.470 Follower
-#Rolle #salofee 4.484 Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
-#Rolle #Name Anzahl Follower
+Other #Rolle #salofee 4.484 Follower
+Company #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Member #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
+Musician #Rolle #Otherme Anzahl Follower
+Artist #Rolle #Otherme Anzahl Follower
+Other #Rolle #Otherme Anzahl Follower
+Company_Staff #Rolle #Otherme Anzahl Follower
+Company #Rolle #Otherme Anzahl Follower
